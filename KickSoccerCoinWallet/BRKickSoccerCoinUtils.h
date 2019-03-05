2f67d5129ce34c9e1a8cb2c9cffb2e82858cf5cb
Your Name <you@example.com>
2019-03-05 23:12:20 +0100

    KSOC

---
+++ KickSoccerCoinWallet/BRKickSoccerCoinUtils.h
@@ -0,0 +1,15 @@
+//
+//  BRKickSoccerCoinUtils.h
+//  kicksoccercoinwallet
+//
+//  Created by furszy on 12/16/17.
+//  Copyright © 2017 Aaron Voisine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRKickSoccerCoinUtils : NSData
+
++ (BOOL)isValidAdress:(NSString*)address;
+
+@end
